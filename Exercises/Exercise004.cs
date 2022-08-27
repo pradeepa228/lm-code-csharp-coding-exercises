de44b244046eb42476eedc3d0dc1d5c9297982c3
pradeepa228 <111800846+pradeepa228@users.noreply.github.com>
2022-08-27 21:58:09 +0100

    Initial Commit to exercise004
    Initial Commit to exercise004

--- Exercises/Exercise004.cs
+++ Exercises/Exercise004.cs
@@ -6,8 +6,8 @@ namespace Exercises
         public static DateTime AddGigaSecond(DateTime dateTime)
         {
             int giga = 1000000000;
-            DateTime d2 = dateTime.AddSeconds(giga);
-            return d2;
+            DateTime dateGiga = dateTime.AddSeconds(giga);
+            return dateGiga;
         }
     }
 }
